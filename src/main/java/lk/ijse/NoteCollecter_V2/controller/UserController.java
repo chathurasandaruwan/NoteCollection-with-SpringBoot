4af7e4ce2b6c4e5e2c1dad880d0f54f81935c0c1
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2024-10-19 11:22:40 +0530

    set Admin role powers

--- src/main/java/lk/ijse/NoteCollecter_V2/controller/UserController.java
+++ src/main/java/lk/ijse/NoteCollecter_V2/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -69,6 +70,7 @@ public class UserController {
         return userService.getSelectedUser(userId);
     }
     @GetMapping(value = "/getAllUsers", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole('ADMIN')")
     public List<UserDTO> getAllUsers() {
         return userService.getAllUsers();
     }
